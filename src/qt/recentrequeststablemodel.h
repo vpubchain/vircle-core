d25b8e1ab7c8dea023bbfdff3f26f3ec973a647d
cekongsb <806138244@qq.com>
2019-06-14 13:59:42 +0800

    modify 'Vp' to 'Vcl' for UI show

--- src/qt/recentrequeststablemodel.h
+++ src/qt/recentrequeststablemodel.h
@@ -51,7 +51,7 @@ private:
 };
 
 /** Model for list of recently generated payment requests / bitcoin: URIs.
- * Vp of wallet model.
+ * Vcl of wallet model.
  */
 class RecentRequestsTableModel: public QAbstractTableModel
 {
