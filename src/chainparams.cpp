900737cf704b04f7b9900593b8ada953eb4ccf75
cekongsb <806138244@qq.com>
2019-08-01 12:45:49 +0800

    fix nNonce

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -483,7 +483,7 @@ public:
         m_assumed_chain_state_size = 1;
 
         //genesis = CreateGenesisBlockMainNet(1500296400, 31429, 0x1f00ffff); // 2017-07-17 13:00:00
-        genesis = CreateGenesisBlockMainNet(1564632000, 24065,  0x1f00ffff); // 2019-08-1 12:00:00
+        genesis = CreateGenesisBlockMainNet(1564632000, 82104,  0x1f00ffff); // 2019-08-1 12:00:00
         consensus.hashGenesisBlock = genesis.GetHash();
         
         bool fNegative;
@@ -688,7 +688,7 @@ public:
         m_assumed_chain_state_size = 1;
 
         //genesis = CreateGenesisBlockTestNet(1502309248, 5924, 0x1f00ffff);
-        genesis = CreateGenesisBlockTestNet(1564632000, 110534, 0x1f00ffff); //2019-05-14
+        genesis = CreateGenesisBlockTestNet(1564632000, 82104, 0x1f00ffff); //2019-05-14
         consensus.hashGenesisBlock = genesis.GetHash();
 	
         bool fNegative;
