81d21440f33488dbc48a80be582474d8699d912a
cekongsb <806138244@qq.com>
2019-08-07 10:17:10 +0800

    fix error

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -792,26 +792,12 @@ public:
         //genesis = CreateGenesisBlockRegTest(1487714923, 0, 0x207fffff);
         genesis = CreateGenesisBlockRegTest(1555488002, 0, 0x207fffff);
         consensus.hashGenesisBlock = genesis.GetHash();
-
-        for (i = 0; i < 4294967295; i++) {
-            genesis.nNonce=i;
-            hash = genesis.GetHash();
-            //std::cout << "hash:" << hash.GetHex() << std::endl;
-            if (UintToArith256(hash) <= bnTarget){
-                //std::cout << "nonce:" << i << std::endl;
-                break;
-            }
-        }
-        hash = genesis.GetHash();
-        if (UintToArith256(hash) <= bnTarget){
-                std::cout << "nonce1:" << i << std::endl;
-        }
         
         std::cout << "block:" << consensus.hashGenesisBlock.GetHex() << std::endl;
         std::cout << "merkle:" << genesis.hashMerkleRoot.GetHex() << std::endl;
         std::cout << "witness:" << genesis.hashWitnessMerkleRoot.GetHex() << std::endl;
         
-        assert(consensus.hashGenesisBlock == uint256S("0xb18c0be1691609a6ff5fa2fe52140a5b4b3363443a910b178e742560ca91c265"));
+        assert(consensus.hashGenesisBlock == uint256S("0xb5b420d66761e672628e32658bab7d235156212c74d0aa7366a77fc477406851"));
         assert(genesis.hashMerkleRoot == uint256S("0x2af16a74a4cfa57ae22752b495aff4653a9b24be2a42e843d0ed2602a9224d16"));
         assert(genesis.hashWitnessMerkleRoot == uint256S("0x16dc1d3c33b8405083e0292d408052a015806827d52585bc4b02b8feddba7ee6"));
         
