2f410a1faf5a4433c757d483dd55861932945cc8
cekongsb <806138244@qq.com>
2019-08-08 15:11:52 +0800

    fix

--- src/wallet/hdwallet.cpp
+++ src/wallet/hdwallet.cpp
@@ -12526,7 +12526,7 @@ bool CHDWallet::CreateCoinStake(unsigned int nBits, int64_t nTime, int nBlockHei
             //     j++;
             // }
 
-            std::vector<uint8_t> vCfwd(1), &vData = *txNew.vpout[0]->GetPData();    //for benyuan modify 'vpout[0]' to 'vpout[1]'
+            std::vector<uint8_t> vCfwd(1), &vData = *txNew.vpout[0]->GetPData();    //mabey if for benyuan modify 'vpout[0]' to 'vpout[1]'
             vCfwd[0] = DO_DEV_FUND_CFWD;
             if (0 != PutVarInt(vCfwd, nDevCfwd)) {
                 return werror("%s: PutVarInt failed: %d.", __func__, nDevCfwd);
