d787ad0c7353d6f172e416e634e3be878ef29f6a
cekongsb <806138244@qq.com>
2019-09-03 16:54:06 +0800

    fix gensis hash

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -435,9 +435,9 @@ public:
         std::cout << "merkle:" << genesis.hashMerkleRoot.GetHex() << std::endl;
         std::cout << "witness:" << genesis.hashWitnessMerkleRoot.GetHex() << std::endl;
 	
-        assert(consensus.hashGenesisBlock == uint256S("0x000050a15fa0180a9aab85f0b478062cd5194066c30c8d8f0699efa62202e103"));
-        assert(genesis.hashMerkleRoot == uint256S("0x1ebe3bd936f4e6a1f9bd24ca8541524b4d5d157b2c05d9f945e557d06423718e"));
-        assert(genesis.hashWitnessMerkleRoot == uint256S("0xd96daf818c5a72ac02186d189b42d3a1f1ccd4f3c17fee639d5e8d167eb769f6"));
+        assert(consensus.hashGenesisBlock == uint256S("0x000016283698bdb0d280366292c11783c8b927df5ddd3245bc2b3176af0ec9ab"));
+        assert(genesis.hashMerkleRoot == uint256S("0x6311aae6f904f5763516bad95209474b3c288d6445b79639250bc83fbd178b09"));
+        assert(genesis.hashWitnessMerkleRoot == uint256S("0x502914c069fdaa749e32483b09f9f505dcbc9fef5673ed0464f14265cdd32435"));
 
         // Note that of those which support the service bits prefix, most only support a subset of
         // possible options.
@@ -491,7 +491,7 @@ public:
 
         checkpointData = {
             {
-                { 0,       uint256S("0x000050a15fa0180a9aab85f0b478062cd5194066c30c8d8f0699efa62202e103")},
+                { 0,       uint256S("0x000016283698bdb0d280366292c11783c8b927df5ddd3245bc2b3176af0ec9ab")},
                 // { 5000,    uint256S("0xebfd13a8143bf9b543474e87d38190036312d7e7ef05e28d6b0aa836b04c6b74")},
                 // { 10000,    uint256S("0x50bd73fc4fdfdfcad973053d9811e136abfe0adb1e201e4a87474d7ccc184871")},
                 // { 15000,    uint256S("0x34c882a588c22f771d311fb34eb12377f840dd72ac28fd3a4ce90e5cf1025023")},
