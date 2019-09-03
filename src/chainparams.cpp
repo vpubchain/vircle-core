45c0397292d53e7db6463b51edcbf05d9ae112dc
cekongsb <806138244@qq.com>
2019-09-03 12:32:12 +0800

    fix nounce

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -403,7 +403,7 @@ public:
         m_assumed_blockchain_size = 1;
         m_assumed_chain_state_size = 1;
 
-        genesis = CreateGenesisBlockMainNet(1564632000, 100456,  0x1f00ffff); // 2019-08-1 12:00:00
+        genesis = CreateGenesisBlockMainNet(1564632000, 26534,  0x1f00ffff); // 2019-08-1 12:00:00
         consensus.hashGenesisBlock = genesis.GetHash();
         
         bool fNegative;
