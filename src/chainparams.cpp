6ddc3ca3c33a8a4a36b75b03dd2f40c0c67c9162
cekongsb <806138244@qq.com>
2019-09-03 16:32:39 +0800

    new chain

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -287,7 +287,7 @@ static CBlock CreateGenesisBlockMainNet(uint32_t nTime, uint32_t nNonce, uint32_
     // Community Initative 
     // RUew9wyHqKkyzBBRdMRucVmyXzi4z89KZm
     out = MAKE_OUTPUT<CTxOutStandard>();
-    out->nValue = 9990000 * COIN;
+    out->nValue = 9980000 * COIN;
     out->scriptPubKey = CScript() << OP_HASH160 << ParseHex("d487155eb3003c6510561afa11207439b8660d3") << OP_EQUAL;
     txNew.vpout.push_back(out);
 
