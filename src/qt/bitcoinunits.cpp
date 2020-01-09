e65133b4ecd15b89db454e9320a2c58d62d92136
cekonsb <806138244@qq.com>
2020-01-09 16:08:00 +0800

    modify from 'μWCL' to 'μTBC' and from 'wcls' to tbcs ...

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -44,7 +44,7 @@ QString BitcoinUnits::longName(int unit)
     {
     case BTC: return QString("TBC");
     case mBTC: return QString("mTBC");
-    case uBTC: return QString::fromUtf8("μWCL (wcls)");
+    case uBTC: return QString::fromUtf8("μTBC (tbcs)");
     case SAT: return QString("Satoshi (sat)");
     default: return QString("???");
     }
@@ -54,7 +54,7 @@ QString BitcoinUnits::shortName(int unit)
 {
     switch(unit)
     {
-    case uBTC: return QString::fromUtf8("wcls");
+    case uBTC: return QString::fromUtf8("tbcs");
     case SAT: return QString("sat");
     default:   return longName(unit);
     }
@@ -66,7 +66,7 @@ QString BitcoinUnits::description(int unit)
     {
     case BTC: return QString("Vircles");
     case mBTC: return QString("Milli-Vircles (1 / 1" THIN_SP_UTF8 "000)");
-    case uBTC: return QString("Micro-Vircles (wcls) (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case uBTC: return QString("Micro-Vircles (tbcs) (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
     case SAT: return QString("Satoshi (sat) (1 / 100" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
     default: return QString("???");
     }
