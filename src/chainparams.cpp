c382522cca6d35a963847167f64b25fc7be9c4cd
cekongsb <806138244@qq.com>
2020-02-03 21:25:44 +0800

    modify nonce

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -404,7 +404,7 @@ public:
         m_assumed_blockchain_size = 1;
         m_assumed_chain_state_size = 1;
 
-        genesis = CreateGenesisBlockMainNet(1580736600, 27574,  0x1f00ffff); // 2020-01-01 00:00:00      
+        genesis = CreateGenesisBlockMainNet(1580736600, 1015,  0x1f00ffff); // 2020-02-03 21:30:00      
         consensus.hashGenesisBlock = genesis.GetHash();
         
         bool fNegative;
